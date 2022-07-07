4926cdd29028f6f6aa7b3d99a2469b591afd35f4
frogizz31 <108894555+frogizz31@users.noreply.github.com>
2022-07-07 20:20:08 +0200

    Update script_install.sh

--- script_install.sh
+++ script_install.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-apt -y update
-apt -y upgrade
-apt install python3 -y
-pip3 install GPUtil
-pip3 install requests
-mkdir /home/user/script
-cd /home/user/script
-wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
-tar -xvf gdrive_2.1.1_linux_386.tar.gz
-./gdrive about
-./gdrive download test.py
+apt -y update &&
+apt -y upgrade &&
+apt install python3 -y &&
+pip3 install GPUtil &&
+pip3 install requests &&
+mkdir /home/user/script &&
+cd /home/user/script &&
+wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz &&
+tar -xvf gdrive_2.1.1_linux_386.tar.gz &&
+./gdrive about &&
+./gdrive download test.py &&
 python3 test.py
