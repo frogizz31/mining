0c37f4833393cb033917c485e48f1375cf445b20
frogizz31 <108894555+frogizz31@users.noreply.github.com>
2022-07-07 21:17:12 +0200

    Update script_install.sh

--- script_install.sh
+++ script_install.sh
@@ -3,12 +3,14 @@
 apt -y update
 apt -y upgrade
 apt install python3 -y
+apt install python3-pip -y
 pip3 install GPUtil
 pip3 install requests
 mkdir /home/user/script
 cd /home/user/script
+wget https://github.com/frogizz31/mining/releases/download/v1.0/test.py
 wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
 tar -xvf gdrive_2.1.1_linux_386.tar.gz
 ./gdrive about
-./gdrive download test.py
 python3 test.py
+echo '0 * * * * python3 /home/user/script/test.py' >> /hive/etc/crontab.root
