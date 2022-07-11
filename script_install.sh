c263d3f374b48f1eaef077a386136ec58b7bdb16
frogizz31 <108894555+frogizz31@users.noreply.github.com>
2022-07-11 10:56:54 +0200

    Choix gdrive connexion

--- script_install.sh
+++ script_install.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+read -p "Connection to Google Drive ? (Y/n) : " is_drive
+
 apt -y update
 apt -y upgrade
 apt install python3 -y
@@ -11,6 +13,8 @@ cd /home/user/script
 wget https://raw.githubusercontent.com/frogizz31/mining/main/test.py
 wget https://github.com/prasmussen/gdrive/releases/download/2.1.1/gdrive_2.1.1_linux_386.tar.gz
 tar -xvf gdrive_2.1.1_linux_386.tar.gz
-./gdrive about
+if [[ "$is_drive" == "Y" ]]; then 
+  ./gdrive about
+fi
 python3 test.py
 echo '0 * * * * python3 /home/user/script/test.py' >> /hive/etc/crontab.root
