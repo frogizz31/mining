5aad96b258f142963e23b47183fa7b39f79637e8
frogizz31 <108894555+frogizz31@users.noreply.github.com>
2022-09-15 20:34:19 +0200

    Update test.py

--- test.py
+++ test.py
@@ -136,7 +136,7 @@ def get_info_whattomine(list_mining_info):
 						"UBQ" : "173",
 					  }
 
-	objectif_vente = { "
+	objectif_vente = { 
 						"RVN" : 0.23515,
 						"FLX" : 3.15,
 						"ETC" : 100,
